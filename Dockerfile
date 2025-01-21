62da91d3ea4f72c58432803944bf0b1bfa53edfa
aidant <15520814+aidant@users.noreply.github.com>
2025-01-21 22:21:43 +1100

    fix deployment

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,8 @@
-FROM lukemathwalker/cargo-chef:latest-rust-1 AS chef
+FROM rust:1.84.0 AS build
 WORKDIR /app
+COPY . /app
+RUN cargo build --release
 
-FROM chef AS planner
-COPY . .
-RUN cargo chef prepare --recipe-path recipe.json
-
-FROM chef AS builder
-COPY --from=planner /app/recipe.json recipe.json
-# Build dependencies - this is the caching Docker layer!
-RUN cargo chef cook --release --recipe-path recipe.json
-# Build application
-COPY . .
-RUN cargo build --release --bin rcon-demo
-
-# We do not need the Rust toolchain to run the binary!
-FROM debian:bookworm-slim AS runtime
-WORKDIR /app
-COPY --from=builder /app/target/release/rcon-demo /usr/local/bin
-ENTRYPOINT ["/usr/local/bin/rcon-demo"]
+FROM gcr.io/distroless/cc-debian12
+COPY --from=build /app/target/release/acorn /
+CMD ["./acorn"]
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use anyhow::Context;
-use packet::RconPacket;
-use rcon_minecraft::Minecraft;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+use {
+    anyhow::Context,
+    packet::RconPacket,
+    rcon_minecraft::Minecraft,
+    tokio::{
+        io::{AsyncReadExt, AsyncWriteExt},
+        net::TcpListener,
+    },
+    tracing::{debug, error, info},
 };
 
 mod packet;
@@ -11,37 +14,67 @@ mod rcon_minecraft;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let mc = TcpListener::bind("0.0.0.0:25575").await?;
+    tracing_subscriber::fmt::init();
+
+    let mc = TcpListener::bind("0.0.0.0:25575").await.map_err(|err| {
+        error!("[acorn] error {}", err);
+        err
+    })?;
+
+    info!("[acorn] listening on tcp://0.0.0.0:25575");
 
     loop {
-        let (mut socket, _) = mc.accept().await?;
+        let (mut socket, addr) = mc.accept().await.map_err(|err| {
+            error!("[acorn] error {}", err);
+            err
+        })?;
+        debug!("[acorn] accepting {}", addr);
         let mut minecraft = Minecraft::new();
 
         tokio::spawn(async move {
             let mut buf = vec![0; 8192];
 
             loop {
-                let len = socket.read(&mut buf).await?;
+                let len = socket.read(&mut buf).await.map_err(|err| {
+                    error!("[acorn] error {}", err);
+                    err
+                })?;
 
                 if len == 0 {
                     continue;
                 }
 
                 if let Some(rcon_packet_request) = RconPacket::deserialize(&mut buf) {
+                    debug!(
+                        "[acorn] Request {{ source: {}, id: {}, type: {:?} }}",
+                        addr, rcon_packet_request.id, rcon_packet_request.r#type
+                    );
+
                     let rcon_packet_response = minecraft
                         .handle_rcon_packet(rcon_packet_request.clone())
                         .await
-                        .context("unable to respond to rcon packet")?;
+                        .context("unable to respond to rcon packet")
+                        .map_err(|err| {
+                            error!("[acorn] error {}", err);
+                            err
+                        })?;
 
-                    println!("rcon_packet_request: {:?}", rcon_packet_request);
-                    println!("rcon_packet_response: {:?}", rcon_packet_response);
+                    debug!(
+                        "[acorn] Response {{ source: {}, id: {}, type: {:?} }}",
+                        addr, rcon_packet_response.id, rcon_packet_response.r#type
+                    );
 
                     socket
                         .write_all(rcon_packet_response.serialize().as_slice())
-                        .await?;
+                        .await
+                        .map_err(|err| {
+                            error!("[acorn] error {}", err);
+                            err
+                        })?;
                 }
             }
 
+            #[allow(unreachable_code)]
             Ok::<(), anyhow::Error>(())
         });
     }
